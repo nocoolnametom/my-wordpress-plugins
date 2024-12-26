b8b42198952158f0f7321628fa8855b84cdde06b
Tom Doggett <810877+nocoolnametom@users.noreply.github.com>
2024-12-26 12:35:48 -0500

    Initial plugins added

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "The Wordpress Plugins I used for WP Servers";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/release-24.11";
+    };
+
+    # Pre-commit hooks
+    pre-commit-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }@inputs:
+    let
+      inherit (self) outputs;
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "x86_64-linux" # Most other systems
+        "aarch64-linux" # Raspberry Pi 4
+        "aarch64-darwin" # Apple Silicon
+      ];
+      inherit (nixpkgs) lib;
+      specialArgs = {
+        inherit
+          inputs
+          outputs
+          nixpkgs
+          ;
+      };
+    in
+    {
+      # Custom packages to be shared or upstreamed.
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        import ./pkgs { inherit inputs pkgs; }
+      );
+
+      checks = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        import ./checks { inherit inputs system pkgs; }
+      );
+
+      # Nix formatter available through 'nix fmt' https://github.com/NixOS/nixfmt
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-rfc-style);
+    };
+}
