8dda6ed80c56aa23244dbdd881581ef4ed0249b0
Tom Doggett <810877+nocoolnametom@users.noreply.github.com>
2026-07-30 12:15:34 -0400

    Updated packages, removed akismet in favor of using unstable or bleeding

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,29 +5,27 @@ let
     # To get a hash run:
     # nix-prefetch-url --unpack https://downloads.wordpress.org/plugins/<name>.<version>.zip
     classic-editor = {
-      version = "1.6.7";
-      sha256 = "0fs60rdkbddijxga1zn7vlpcmnmvwrdgfvn0wm6y4cxg2cqn1987";
+      version = "1.7.0";
+      sha256 = "0vhrglvs6q7wnkph88gy388sbja3904740blm2ipli5ddy4fmk15";
     };
     column-shortcodes = {
+      # Abandoned
       version = "1.0.1";
       sha256 = "04bsr02iazj2indwdg6nrjn9dszknvci39899kz0g3kbn4wgv2f3";
     };
     simple-csv-tables = {
+      # Abandoned
       version = "1.0.3";
       url = "https://downloads.wordpress.org/plugins/simple-csv-tables.zip";
       sha256 = "0aghic2mxljbwir35rvgbz3s0zwys4844svjnmlpad2l8viwvabf";
     };
     youtube-embed-plus = {
-      version = "14.2.1.3";
-      sha256 = "13wzxnp3wkw3p2nrqbjqvd9qn4g9dfzpmfzxps80sz2ljj214ar6";
+      version = "14.2.6";
+      sha256 = "0715ma73m6wv2g7v3ny2v0zkzign9rrmn0fg6bw23m4lizw3sfcw";
     };
     wpdatatables = {
-      version = "3.4.2.37";
-      sha256 = "031rvkahda1czi6gl0wihzakn3jx74gm45r64kvsmbxfkmf6mh42";
-    };
-    akismet = {
-      version = "5.3.3";
-      sha256 = "0lidw16nw5lp0f1xijfaxw168av0j0ikwxwxj905ar46a87r5s36";
+      version = "6.5.1.3";
+      sha256 = "1drnss9l8pvq36g5a0x03w4qg89cxgmg394yjmhigmygf6syw87m";
     };
   };
   mkPlugin =
